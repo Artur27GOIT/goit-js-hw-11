cf09123b032ddd521920038bd6a767f9086d467e
Artur27 <arturfurman7@gmail.com>
2026-01-17 21:53:09 +0100

    done

---
+++ src/js/render-functions.js
@@ -0,0 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import 'loaders.css/loaders.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+if (!loaderEl) {
+  console.warn('Loader element not found: .loader');
+} else {
+  console.log('Loader element found:', loaderEl);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      image => `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('is-hidden');
+}
---
+++ src/main.js
@@ -0,0 +1,62 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'loaders.css/loaders.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const formEl = document.querySelector('.form');
+
+if (formEl) {
+  formEl.addEventListener('submit', onSearch);
+} else {
+  console.warn('Form element not found: .form');
+}
+
+function onSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const input = form.elements['search-text'];
+  const query = input.value.trim();
+
+  if (query === '') {
+    iziToast.error({
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  form.reset();
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      const hits = data && data.hits ? data.hits : [];
+      if (hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      createGallery(hits);
+    })
+    .catch(error => {
+      console.error(error);
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
